79f90878cb793246dc3254c5a5f8fdbd88a50b65
Fer <zetter@Fers-MacBook-Air.local>
2022-01-27 15:47:10 -0600

    Test 01

--- routes/form.js
+++ routes/form.js
@@ -4,25 +4,28 @@ const mailer = require('@modules/mailer.js')
 module.exports = app => {
 
   app.get('/form', (req, res) => {
-    res.render('form', { title: 'DSP Email Sending', recruiterEmail: req.user.email })
+    console.log(req.user)
+    const recruiterEmail = (req.user ? req.user.email : '')
+    res.render('form', { title: 'DSP Email Sending', recruiterEmail: recruiterEmail })
   })
 
   app.post('/form', async (req, res) => {
     const body = req.body
-    if (body.prosArray) body.prosArray = body.prosArray.split('\r\n')
-    if (body.consArray) body.consArray = body.consArray.split('\r\n')
-    if (body.scheduleArray) body.scheduleArray = body.scheduleArray.split('\r\n')
-
-    const response = await mailer.sendEmail(body.email, req.user, {
-      replyTo: body.replyTo,
-      subject: 'Simple Transactional Email',
-      context: body
-    })
-
-    res.render('message', {
-      recruiterEmail: req.user.email,
-      candidateEmail: body.email
-    })
+    res.status(200).send(req.user)
+    // if (body.prosArray) body.prosArray = body.prosArray.split('\r\n')
+    // if (body.consArray) body.consArray = body.consArray.split('\r\n')
+    // if (body.scheduleArray) body.scheduleArray = body.scheduleArray.split('\r\n')
+    //
+    // const response = await mailer.sendEmail(body.email, req.user, {
+    //   replyTo: body.replyTo,
+    //   subject: 'Simple Transactional Email',
+    //   context: body
+    // })
+    //
+    // res.render('message', {
+    //   recruiterEmail: req.user.email,
+    //   candidateEmail: body.email
+    // })
   })
 
 }
