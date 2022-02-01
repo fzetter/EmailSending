9a75419eac7f7ea8cf85ece5a540e07219cc7d60
Fer <zetter@Fers-MacBook-Air.local>
2022-02-01 01:27:02 -0600

    Generate Access Token

--- modules/mailer.js
+++ modules/mailer.js
@@ -5,20 +5,40 @@ const keys = require('@config/config')
 const { google } = require("googleapis")
 const OAuth2 = google.auth.OAuth2
 
+const axios = require('axios')
+const querystring = require('querystring')
+
+const getAccessToken = async refreshToken => {
+  try {
+    const accessTokenObj = await axios.post(
+      'https://www.googleapis.com/oauth2/v4/token',
+      querystring.stringify({
+        refresh_token: refreshToken,
+        client_id: keys.googleClientID,
+        client_secret: keys.googleClientSecret,
+        grant_type: 'refresh_token'
+      })
+    )
+    return accessTokenObj.data.access_token
+  } catch (err) { console.log(err) }
+}
+
 module.exports.sendEmail = (emails, user, params) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
 
     let transporter, hbsOptions, mailOptions
 
-    // Obtain An Access Token
-    const oauth2Client = new OAuth2 (
-      keys.googleClientID,
-      keys.googleClientSecret,
-      'https://developers.google.com/oauthplayground'
-    )
+    // // Obtain An Access Token
+    // const oauth2Client = new OAuth2 (
+    //   keys.googleClientID,
+    //   keys.googleClientSecret,
+    //   'https://developers.google.com/oauthplayground'
+    // )
+    //
+    // oauth2Client.setCredentials({ access_token: user.accessToken, refresh_token: user.refreshToken, expiry_date: true })
+    // const accessToken = oauth2Client.refreshAccessToken()
 
-    oauth2Client.setCredentials({ access_token: user.accessToken, refresh_token: user.refreshToken, expiry_date: true })
-    const accessToken = oauth2Client.refreshAccessToken()
+    const accessToken = await getAccessToken(user.refreshToken)
 
     // SMTP Transport
     transporter = nodemailer.createTransport({
